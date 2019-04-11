9583a1d0e8d5b1e3d3264a930010da972f94d574
dnyaneshm36 <dnyaneshm36@gmail.com>
2019-04-12 00:49:46 +0530

    Update b+.c
    search commplete commit

--- bst_tree/b+.c
+++ bst_tree/b+.c
@@ -1,112 +1,143 @@
 #include<stdio.h>
-#includ<stdlib.h>
+#include<stdlib.h>
 typedef enum {FAILURE,SUCCESS}status_code;
-typedef struct tree_tag
-{
-  int data ;
-  struct tree_tag * left;
-  struct tree_tag * right;
-}tree_node;
+
 typedef struct dblist
 {
   int data;
-  struct dblist * prev;
-  struct dblist * next;
-}dblit_type;
-void visit(tree_node * ptr)
+  struct dblist *prev;
+  struct dblist *next;
+}dblist_type;
+typedef struct btree_tag
 {
- 	if(ptr!=NULL)
- 	{
- 	 printf("%d",ptr->data);
- 	}
-}
-void preorder(tree_node *root)
+  int k[4];
+  int count;
+  struct btree_tag *p[5];
+  struct dblist *db[5];
+}btree_node;
+
+btree_node * give_empty_btree_node()
 {
- 	if(root!=NULL)
- 	{
- 	visit(root);
- 	preorder(root->left);
- 	preorder(root->right);
- 	}
+  int i;
+  btree_node * new;
+  new =(btree_node *)malloc(sizeof(btree_node));
+  new->count=0;
+  for(i=0;i<4;i++)
+  {
+    new->k[i]=-1;
+    new->db[i]=NULL;
+    new->p[i]=NULL;
+  }
+  new->db[4]=NULL;
+  new->p[4]=NULL;
+  return new;
 }
-void inorder(tree_node *root)
+
+dblist_type * search_for_record(btree_node * root ,int key)
 {
- 	if(root!=NULL)
- 	{
- 	 inorder(root->left);
- 	 visit(root);
- 	 inorder(root->right);
- 	}
+  int i,done,flag=0;
+  btree_node *ptr=root;
+  dblist_type *ret_val;
+  while (flag==0)
+  {
+      done=0;
+      for(i=0;i<4&&done==0&&(ptr->k[i])!=-1&&i<(ptr->count);i++)     //tavaling in key array of size 4
+      {                                     //stoping when we get empty space which is -1
+        if (key<ptr->k[i])              //stoping when key is less than array(key) of i
+        {                               //count element to extra protection
+          done=1;
+        }
+      }                                     //exiting loop i++ is perform
+      if(done==1)                          //decrease beacause of lesserkeys
+      {                                   //found at pointer array of btree_tag
+        i--;
+      }
+      if(ptr->p[0]!=NULL)          //to check weather this btree_node is not last
+      {
+        ptr=ptr->p[i];
+      }
+      else                       //going for dblist_type list
+      {                          //that mean record nodes
+        flag=1;
+      }
+  }
+    ret_val=ptr->db[i];
+    for(i=0;i<4&&done==0;i++)  //at most we are cheacking next  4 record
+    {
+      if(ret_val->data==key)
+      {
+        done=1;              //got it what i want
+      }
+      else if(key>ret_val->data&&ret_val->next!=NULL) //key should greater current record
+      {                                           // beacause chances geting record
+        ret_val=ret_val->next; //
+      }
+    }
+    if(done==0) //we not found in next 4 record
+    {
+      printf("\nerror :-not found element or tree is not remain bst tree\n" );
+      ret_val=NULL;
+    }
+    return ret_val;
 }
-void postorder(tree_node * root)
+btree_node* insert_record(btree_node * root ,dblist_type* newnode)
 {
- 	if(root)
- 	{
- 	 postorder(root->left);
- 	 postorder(root->right);
- 	 visit(root);
- 	}
+  int i,done,flag=0;
+  btree_node *ptr,*temp;
+  dblist_type *ret_val;
+  int no_list_node;
+  ptr=root;
+  if(ptr==NULL) //empty tree
+  {
+    temp=give_empty_btree_node(); //give empty btree_node
+    temp->k[0]=newnode->data;
+    temp->db[0]=newnode;
+    root= temp;       //root changes to btree_node
+  }
+  else
+  {
+    while (flag==0)
+    {
+        done=0;
+        for(i=0;i<4&&done==0&&(ptr->k[i])!=-1&&i<(ptr->count);i++)     //tavaling in key array of size 4
+        {                                     //stoping when we get empty space which is -1
+          if ((new->data)<ptr->k[i])              //stoping when key is less than array(key) of i
+          {                               //count element to extra protection
+            done=1;
+          }
+        }                                     //exiting loop i++ is perform
+        if(done==1)                          //decrease beacause of lesserkeys
+        {                                   //found at pointer array of btree_tag
+          i--;
+        }
+        if(ptr->p[0]!=NULL)          //to check weather this btree_node is not last
+        {
+          ptr=ptr->p[i];
+        }
+        else                       //going for dblist_type list
+        {                          //that mean record nodes
+          flag=1;
+        }
+    }
+    ret_val=ptr->db[i];
+    for(i=0;i<4&&done==0;i++)  //at most we are cheacking next  4 record from bottom list
+    {
+      if(ret_val->data==key)
+      {
+        done=1;              //got it what i want
+      }
+      else if(key>ret_val->data&&ret_val->next!=NULL) //key should greater current record
+      {                                           // beacause chances geting record
+        ret_val=ret_val->next; //
+      }
+    }
+  }
 }
-int countnodes(tree_node * root)
-{
-	int ret_val;
-	if(root)
-	{
-	ret_val=0;
-	}
-	else
-	{
-	 ret_val=1+countnodes(root->left)+countnodes(root->right);
-	}
-  return ret_val;
-}
-int countleaves(tree_node * root)
-{
-	int ret_val;
-	if(root!=NULL)
-	{
-	 ret_val=0;
-	}
-	else if(root->left!=NULL&&root->right!=NULL)
-	{
-	ret_val=1;
-	}
-	else
-	{
-	 ret_val=countleaves(root->left)+countleaves(root->right);
-	}
- return ret_val;
-}
-int height(tree_node* root)
-{
-	int ret_val,left_height,right_height;
-	if(root==NULL)
-	{
-	 ret_val=-1;
-	}
-	else if(root->left==NULL||root->right==NULL) //if we make this condition as comment
-	{						//it will it take many checks end of
-	 ret_val=0;				 //day it will not require.
-	}
-	else
-	{
-	left_height=height(root->left);
-	right_height=height(root->right);
-		if(left_height<right_height)
-		{
-		 ret_val=1+right_height;
-		}
-		else
-		{
-		 ret_val=1+left_height;
-		}
-	}
- return ret_val;
-}
+
 int main()
 {
-  tree_node * root;
+  btree_node * root;
   root=NULL;
-  
+
   return 0;
 }
