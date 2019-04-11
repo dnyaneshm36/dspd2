7a2b67a1099980d6183955670d6107d1e4624392
dnyaneshm36 <dnyaneshm36@gmail.com>
2019-04-12 02:27:20 +0530

    Update b+.c

--- bst_tree/b+.c
+++ bst_tree/b+.c
@@ -32,7 +32,21 @@ btree_node * give_empty_btree_node()
   new->p[4]=NULL;
   return new;
 }
-
+void insert_betn_dbnodes(dblist_type *temp,dblist_type *newnode)
+ {
+  /* code */
+}
+int give_no_of_records_in_list(dblist_type *start,dblist_type *nextstart)
+{
+  int count=0;
+  dblist_type *temp=start;
+  while(temp!=nextstart&&(temp->data<=nextstart->data)) //no nead to give second condition
+  {                                       //but still for extra protection lol.
+    count++;
+    temp=temp->next;
+  }
+  return count;
+}
 dblist_type * search_for_record(btree_node * root ,int key)
 {
   int i,done,flag=0;
@@ -84,7 +98,7 @@ btree_node* insert_record(btree_node * root ,dblist_type* newnode)
 {
   int i,done,flag=0;
   btree_node *ptr,*temp;
-  dblist_type *ret_val;
+  dblist_type *ret_val,*start,*end;
   int no_list_node;
   ptr=root;
   if(ptr==NULL) //empty tree
@@ -101,7 +115,7 @@ btree_node* insert_record(btree_node * root ,dblist_type* newnode)
         done=0;
         for(i=0;i<4&&done==0&&(ptr->k[i])!=-1&&i<(ptr->count);i++)     //tavaling in key array of size 4
         {                                     //stoping when we get empty space which is -1
-          if ((new->data)<ptr->k[i])              //stoping when key is less than array(key) of i
+          if ((newnode->data)<ptr->k[i])              //stoping when key is less than array(key) of i
           {                               //count element to extra protection
             done=1;
           }
@@ -117,19 +131,24 @@ btree_node* insert_record(btree_node * root ,dblist_type* newnode)
         else                       //going for dblist_type list
         {                          //that mean record nodes
           flag=1;
+          start=ptr->db[i];           //both start and end need leter for counting node
+          end=ptr->db[i+1]; //in the dblist. nextstartof node_dblist
         }
     }
-    ret_val=ptr->db[i];
-    for(i=0;i<4&&done==0;i++)  //at most we are cheacking next  4 record from bottom list
+    ret_val=ptr->db[i];                 //start of node_dblist
+    no_list_node=give_no_of_records_in_list(start,end);  //it seding start and start of new list
+    for(i=0;i<no_list_node&&done==0;i++)  //at most we are cheacking next  4 record from bottom list
     {
-      if(ret_val->data==key)
-      {
-        done=1;              //got it what i want
-      }
-      else if(key>ret_val->data&&ret_val->next!=NULL) //key should greater current record
+      if((newnode->data)>ret_val->data&&ret_val->next!=NULL) //node data should greater current record
       {                                           // beacause chances geting record
         ret_val=ret_val->next; //
       }
+    }
+    insert_betn_dbnodes(ret_val,newnode); //this fun insert newnode before ret_val
+    no_list_node++;     //one is added
+    if (no_list_node==5)
+    {
+      //code for spleatting
     }
   }
 }
