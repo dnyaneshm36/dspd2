fa96bf071864fb2451332cb90ea2eb7cb5bbdfc6
dnyaneshm36 <dnyaneshm36@gmail.com>
2019-04-12 21:44:26 +0530

    Update b+.c

--- bst_tree/b+.c
+++ bst_tree/b+.c
@@ -10,10 +10,10 @@ typedef struct dblist
 }dblist_type;
 typedef struct btree_tag
 {
-  int k[4];
+  int k[5];
   int count;
-  struct btree_tag *p[5];
-  struct dblist *db[5];
+  struct btree_tag *p[6];
+  struct dblist *db[6];
 }btree_node;
 
 btree_node * give_empty_btree_node()
@@ -22,19 +22,43 @@ btree_node * give_empty_btree_node()
   btree_node * new;
   new =(btree_node *)malloc(sizeof(btree_node));
   new->count=0;
-  for(i=0;i<4;i++)
+  for(i=0;i<5;i++)
   {
     new->k[i]=-1;
     new->db[i]=NULL;
     new->p[i]=NULL;
   }
-  new->db[4]=NULL;
-  new->p[4]=NULL;
+  new->db[5]=NULL;
+  new->p[5]=NULL;
   return new;
 }
-void insert_betn_dbnodes(dblist_type *temp,dblist_type *newnode)
+status_code insert_betn_dbnodes(dblist_type *temp,dblist_type *newnode)
  {
-  /* code */
+  /*this code is add newnode before temp*/
+  status_code sc=SUCCESS;
+    dblist_type *before;
+  if (temp==NULL||newnode==NULL)
+  {
+    sc=FAILURE;
+  }
+  else
+  {
+      if(temp->prev==NULL)
+      {
+        newnode->next=temp;
+        newnode->prev=NULL;
+        temp->prev=newnode;
+      }
+      else
+      {
+        before =temp->prev;
+        newnode->next=temp;
+        newnode->prev=before;
+        before->next=newnode;
+        temp->prev=newnode;
+      }
+  }
+        return sc;
 }
 int give_no_of_records_in_list(dblist_type *start,dblist_type *nextstart)
 {
@@ -46,6 +70,42 @@ int give_no_of_records_in_list(dblist_type *start,dblist_type *nextstart)
     temp=temp->next;
   }
   return count;
+}
+btree_node * split_btreenode_when_5node_are_there(btree_node *root,int key)
+{
+  int i,done,flag=0;
+  btree_node *ptr=root;
+  if(ptr->cou)
+
+
+
+
+
+
+
+  while (flag==0)
+  {
+      done=0;
+      for(i=0;i<4&&done==0&&(ptr->k[i])!=-1&&i<(ptr->count);i++)     //tavaling in key array of size 4
+      {                                     //stoping when we get empty space which is -1
+        if (key<ptr->k[i])              //stoping when key is less than array(key) of i
+        {                               //count element to extra protection
+          done=1;
+        }
+      }                                     //exiting loop i++ is perform
+      if(done==1)                          //decrease beacause of lesserkeys
+      {                                   //found at pointer array of btree_tag
+        i--;
+      }
+      if(ptr->p[0]!=NULL)          //to check weather this btree_node is not last
+      {
+        ptr=ptr->p[i];
+      }
+      else                       //going for dblist_type list
+      {                          //that mean record nodes
+        flag=1;
+      }
+  }
 }
 dblist_type * search_for_record(btree_node * root ,int key)
 {
@@ -96,7 +156,7 @@ dblist_type * search_for_record(btree_node * root ,int key)
 }
 btree_node* insert_record(btree_node * root ,dblist_type* newnode)
 {
-  int i,done,flag=0;
+  int i,done,flag=0,light;
   btree_node *ptr,*temp;
   dblist_type *ret_val,*start,*end;
   int no_list_node;
@@ -144,11 +204,30 @@ btree_node* insert_record(btree_node * root ,dblist_type* newnode)
         ret_val=ret_val->next; //
       }
     }
-    insert_betn_dbnodes(ret_val,newnode); //this fun insert newnode before ret_val
+    light=insert_betn_dbnodes(ret_val,newnode); //this fun insert newnode before ret_val
     no_list_node++;     //one is added
     if (no_list_node==5)
     {
-      //code for spleatting
+      //code for spleatting db list
+      ret_val=ptr->db[i];  //starting for sublist
+      ret_val=ret_val->next;
+      ret_val=ret_val->next;   //reached at median or middle
+      ptr->count++;            //count of on of nodes are ++
+      for(j=ptr->count;j>i;j--)   //pushing data one location ahead
+      {                         //j can reach upto i+1 there a[i]copyed
+        ptr->k[j]=ptr->k[j-1];     //form j max is count so a[last bit not val(-1)copyed]
+      }
+      a[i]=ret_val->data;       //at i positiion we bring key
+      for(j=(ptr->count)+1;j>i+1;j--)   //similiry pointer form
+      {                                 //i+1pointer copyed to i+2 loca
+        ptr->db[j]=ptr->db[j-1];    //  last db[count] copyed to db[count+1]
+      }
+      ptr->db[i+1]=ret_val;        //at i+1 pointer listed to madian or middle
+          if(ptr->count==5)
+          {
+            //code for spliatint btree nodes
+            root=split_btreenode_when_5node_are_there(root,ptr->k[2])
+          }
     }
   }
 }
